da499ef6d78cda235d62db6ec437dc586b23f222
weerachit <keemkeem207@gmail.com>
2023-07-15 21:22:28 +0700

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ echo "Pulling"
 git pull
 
 echo "Building application"
-docker-compose up -d --build
+docker-compose -f "$DOCKER_COMPOSE_FILE" up -d --build
 
-echo "Executing deploy.sh"
-./deploy.sh  # Make sure to provide the correct path if deploy.sh is not in the current directory
 
-echo "Deployment complete"
