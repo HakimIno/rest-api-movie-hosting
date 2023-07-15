2eb3c040db52177902eccf4cd3c7b618379c2e64
weerachit <keemkeem207@gmail.com>
2023-07-15 21:20:28 +0700

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
-
 echo "Pulling"
 git pull
 
 echo "Building application"
 docker-compose up -d --build
+
+echo "Executing deploy.sh"
+./deploy.sh  # Make sure to provide the correct path if deploy.sh is not in the current directory
+
+echo "Deployment complete"
